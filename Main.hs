eefce7da13f2f6f0693c11333a7fc07874c41415
Ahmad N. Raja <ar@penneo.com>
2017-07-10 00:40:50 +0200

    Made getHeaders a pure function

--- Main.hs
+++ Main.hs
@@ -1,27 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Control.Lens
-import qualified Data.ByteString.Char8 as BS
-
+import qualified Data.ByteString.Char8 as Char8
 import Data.ByteString.Lazy.Internal
 import Network.Wreq
 import System.Environment
 
-getHeaders :: IO Options
-getHeaders = do
-  authToken <- BS.readFile "/home/mandark/.penneo-auth-token-local"
-  return $ defaults & header "x-auth-token" .~ [authToken] &
-    header "Authorization" .~
-    ["JWT"]
+getHeaders :: String -> Options
+getHeaders token =
+  defaults & header "x-auth-token" .~ [Char8.pack token] &
+  header "Authorization" .~
+  ["JWT"]
 
-request :: String -> IO (Response ByteString)
-request url = do
-  headers <- getHeaders
-  getWith headers url
+request :: String -> String -> IO (Response ByteString)
+request token url = getWith (getHeaders token) url
 
 main :: IO ()
 main = do
-  r <- request "http://172.17.42.1:8000/app_dev.php/api/v1/casefiles"
-  -- headers <- getHeaders
-  -- r <- getWith headers "http://172.17.42.1:8000/app_dev.php/api/v1/casefiles"
+  token <- Char8.readFile "/home/mandark/.penneo-auth-token-local"
+  r <-
+    request
+      (Char8.unpack token)
+      "http://172.17.42.1:8000/app_dev.php/api/v1/casefiles"
   print $ r ^. responseStatus . statusCode
