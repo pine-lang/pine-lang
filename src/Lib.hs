ea69d2b14bb3b08368151c925e63d3674281805d
Ahmad N. Raja <ar@penneo.com>
2017-07-15 16:48:34 +0200

    Added functionality for filters

--- src/Lib.hs
+++ src/Lib.hs
@@ -14,17 +14,25 @@ import Network.Wreq
 
 type Token = String
 
-type Id = Int
+type BaseUrl = String
+
+type RelativeUrl = String
 
 type Url = String
+
+type Filter = Either Id Title
+
+type Id = Int
+
+type Title = String
 
 getOptions :: Token -> Options
 getOptions token =
   defaults & (header "x-auth-token" .~ [Char8.pack token]) &
   (header "Authorization" .~ ["JWT"])
 
-request :: (Url, Options) -> IO (Response ByteString)
-request (url, options) = getWith options url
+request :: Url -> Options -> IO (Response ByteString)
+request url options = getWith options url
 
 extract :: Response a -> a
 extract response = response ^. responseBody
@@ -32,26 +40,38 @@ extract response = response ^. responseBody
 extractList :: Response [a] -> [a]
 extractList response = response ^. responseBody
 
-getCaseFile :: (Url, Options) -> Id -> IO CF.CaseFile
-getCaseFile (baseUrl, options) id = do
-  response <-
-    asJSON =<< request (baseUrl ++ "casefiles/" ++ (show id), options) :: IO (Response CF.CaseFile)
+url :: BaseUrl -> RelativeUrl -> Maybe Filter -> Url
+url base relative filter =
+  base ++
+  relative ++
+  case filter of
+    Just (Left id) -> "/" ++ (show id)
+    Just (Right title) -> "?title=" ++ title
+    _ -> ""
+
+getCaseFiles :: BaseUrl -> Options -> Maybe Filter -> IO CF.CaseFile
+getCaseFiles baseUrl options filter = do
+  response <- asJSON =<< request (url baseUrl "casefiles" filter) options
   return $ extract response
 
-getDocuments :: (Url, Options) -> CF.CaseFile -> IO [D.Document]
-getDocuments (baseUrl, options) caseFile = do
+getDocuments :: BaseUrl -> Options -> CF.CaseFile -> IO [D.Document]
+getDocuments baseUrl options caseFile = do
   response <-
     asJSON =<<
     request
-      ( baseUrl ++ "casefiles/" ++ (show (CF.id caseFile)) ++ "/documents"
-      , options) :: IO (Response [D.Document])
+      (url
+         baseUrl
+         ("casefiles/" ++ (show (CF.id caseFile)) ++ "/documents")
+         Nothing
+      )
+      options
   return $ extractList response
 
-run :: Token -> Url -> IO [D.Document]
-run token baseUrl =
+run :: Token -> BaseUrl -> IO [D.Document]
+run token url =
   let options = getOptions token
-  in do caseFile <- getCaseFile (baseUrl, options) 1
-        documents <- getDocuments (baseUrl, options) caseFile
+  in do caseFile <- getCaseFiles url options (Just (Left 1))
+        documents <- getDocuments url options caseFile
         return documents
   --
   -- return $ r
