9feeccebb1b53d1a612617605d48870a55f57a4c
Ahmad N. Raja <ar@penneo.com>
2017-07-15 16:57:57 +0200

    Aliases Options to Headers as it make more sense for now

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,27 +12,25 @@ import qualified Model.CaseFile as CF
 import qualified Model.Document as D
 import Network.Wreq
 
+-- Request related
 type Token = String
-
 type BaseUrl = String
-
 type RelativeUrl = String
-
 type Url = String
-
 type Filter = Either Id Title
+type Headers = Options -- wreq calls it Options, will change if doesn't make sense
 
+-- Entity fields
 type Id = Int
-
 type Title = String
 
-getOptions :: Token -> Options
-getOptions token =
+getHeaders :: Token -> Headers
+getHeaders token =
   defaults & (header "x-auth-token" .~ [Char8.pack token]) &
   (header "Authorization" .~ ["JWT"])
 
-request :: Url -> Options -> IO (Response ByteString)
-request url options = getWith options url
+request :: Url -> Headers -> IO (Response ByteString)
+request url headers = getWith headers url
 
 extract :: Response a -> a
 extract response = response ^. responseBody
@@ -40,8 +38,8 @@ extract response = response ^. responseBody
 extractList :: Response [a] -> [a]
 extractList response = response ^. responseBody
 
-url :: BaseUrl -> RelativeUrl -> Maybe Filter -> Url
-url base relative filter =
+getUrl :: BaseUrl -> RelativeUrl -> Maybe Filter -> Url
+getUrl base relative filter =
   base ++
   relative ++
   case filter of
@@ -49,29 +47,28 @@ url base relative filter =
     Just (Right title) -> "?title=" ++ title
     _ -> ""
 
-getCaseFiles :: BaseUrl -> Options -> Maybe Filter -> IO CF.CaseFile
-getCaseFiles baseUrl options filter = do
-  response <- asJSON =<< request (url baseUrl "casefiles" filter) options
+getCaseFiles :: BaseUrl -> Headers -> Maybe Filter -> IO CF.CaseFile
+getCaseFiles baseUrl headers filter = do
+  response <- asJSON =<< request (getUrl baseUrl "casefiles" filter) headers
   return $ extract response
 
-getDocuments :: BaseUrl -> Options -> CF.CaseFile -> IO [D.Document]
-getDocuments baseUrl options caseFile = do
+getDocuments :: BaseUrl -> Headers -> CF.CaseFile -> IO [D.Document]
+getDocuments baseUrl headers caseFile = do
   response <-
     asJSON =<<
     request
-      (url
+      (getUrl
          baseUrl
          ("casefiles/" ++ (show (CF.id caseFile)) ++ "/documents")
-         Nothing
-      )
-      options
+         Nothing)
+      headers
   return $ extractList response
 
 run :: Token -> BaseUrl -> IO [D.Document]
 run token url =
-  let options = getOptions token
-  in do caseFile <- getCaseFiles url options (Just (Left 1))
-        documents <- getDocuments url options caseFile
+  let headers = getHeaders token
+  in do caseFile <- getCaseFiles url headers (Just (Left 1))
+        documents <- getDocuments url headers caseFile
         return documents
   --
   -- return $ r
