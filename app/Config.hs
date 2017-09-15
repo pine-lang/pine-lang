159a23f8735c689f6ad32c42d6ede0d6dfe93129
Ahmad N. Raja <ar@penneo.com>
2017-09-16 00:27:53 +0200

    Using Symlink for configuration

--- app/Config.hs
+++ app/Config.hs
@@ -1,15 +1,1 @@
-module Config
-  ( connectInfo
-  ) where
-
-import Database.MySQL.Simple
-
-connectInfo :: ConnectInfo
-connectInfo = ConnectInfo { connectHost = "?",
-                            connectPort = 3306,
-                            connectUser = "?",
-                        connectPassword = "?",
-                        connectDatabase = "?",
-                         connectOptions = [],
-                            connectPath = "",
-                             connectSSL = Nothing }
+Config.template.hs
