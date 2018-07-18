b35e735dc60471ab9c858df328bd0f58fa635442
Ahmad N. Raja <ar@penneo.com>
2018-07-18 18:07:29 +0200

    Make sure that we always pass the db config except for helpers

--- src/pine/db.clj
+++ src/pine/db.clj
@@ -4,6 +4,52 @@
             [clojure.string :as s])
   )
 
+(defn exec
+  "Execute raw sql queries"
+  [db query]
+  (->> query
+       (j/query db)))
+
+(defn escape
+  "Remove non alphanumeric chars from a string"
+  [s]
+  (s/replace s #"[^A-Za-z0-9_-]*" ""))
+
+(defn table-definition
+  "Create table definition"
+  [db table]
+  (->> table
+      escape
+      (format "show create table %s")
+      (exec db)
+      first
+      ((keyword "create table"))
+      ))
+
+(defn references
+  "Get the tables used in the foreign keys"
+  [db table]
+  (->> table
+       (table-definition db)
+       (re-seq #"FOREIGN KEY .`(.*)`. REFERENCES `(.*?)`")
+       (map (fn [[_ col t]] { (keyword t) col }))
+       (apply merge)
+       ))
+
+(defn relation
+  "Get the column that has the relationship between the tables:
+  (relation \"caseFiles\" :owns \"documents\") => \"caseFileId\"
+  (relation \"documents\" :owned-by \"caseFile\") => \"caseFileId\"
+  "
+  [db t1 relationship t2]
+  (case relationship
+    :owns     ((keyword t1) (references db t2))
+    :owned-by ((keyword t2) (references db t1))
+    :else     nil)
+  )
+
+;; Helpers
+
 (defn $
   "Execute raw sql queries:
   ($ \"select id from users limit 10\")
@@ -15,41 +61,3 @@
         fn))
   ([query]
    ($ (fn[x] x) query)))
-
-(defn escape
-  "Remove non alphanumeric chars from a string"
-  [s]
-  (s/replace s #"[^A-Za-z0-9_-]*" ""))
-
-(defn table-definition
-  "Create table definition"
-  [table]
-  (->> table
-      escape
-      (format "show create table %s")
-      $
-      first
-      ((keyword "create table"))
-      ))
-
-(defn references
-  "Get the tables used in the foreign keys"
-  [table]
-  (->> table
-       table-definition
-       (re-seq #"FOREIGN KEY .`(.*)`. REFERENCES `(.*?)`")
-       (map (fn [[_ col t]] { (keyword t) col }))
-       (apply merge)
-       ))
-
-(defn relation
-  "Get the column that has the relationship between the tables:
-  (relation \"caseFiles\" :owns \"documents\") => \"caseFileId\"
-  (relation \"documents\" :owned-by \"caseFile\") => \"caseFileId\"
-  "
-  [t1 relationship t2]
-  (case relationship
-    :owns     ((keyword t1) (references t2))
-    :owned-by ((keyword t2) (references t1))
-    :else     nil)
-  )
