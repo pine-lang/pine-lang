37a46f6905eb09a273064ef9bce9a2d34c800c16
Ahmad N. Raja <ar@penneo.com>
2019-04-21 19:48:42 +0200

    Added basic tests for the api handler functionality

---
+++ test/pine/handler_test.clj
@@ -0,0 +1,23 @@
+(ns pine.handler-test
+  (:require [clojure.test :refer :all]
+            [pine.handler :refer :all]
+            [clojure.test :as t]
+            [pine.fixtures :as fixtures]
+            ))
+
+
+(deftest build-query:single-resource-with-id
+  (testing "Build a query using a single resource"
+    (is
+     (=
+      "SELECT customers_0.* FROM customers AS customers_0 WHERE (customers_0.id = 1) LIMIT 50;"
+      (build-query fixtures/schema "customers 1")
+      ))))
+
+(deftest build-query:single-resource-with-condition
+  (testing "Build a query using a single resource"
+    (is
+     (=
+      "SELECT customers_0.* FROM customers AS customers_0 WHERE (customers_0.name = \"Acme Inc\") LIMIT 50;"
+      (build-query fixtures/schema "customers name='Acme Inc'")
+      ))))
