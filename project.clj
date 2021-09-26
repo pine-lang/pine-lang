27836f66f3643be6e155402f7701b539ee5ca204
Ahmad Nazir Raja <ahmadnazir@gmail.com>
2021-09-26 19:37:26 +0200

    refactor: sorted project dependencies
    Good to see CIDER refactor working ... fun! :D
    
    ```
    , r s p
    ```

--- project.clj
+++ project.clj
@@ -3,26 +3,22 @@
   :url "https://github.com/ahmadnazir/pine"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [
-                 [org.clojure/clojure "1.8.0"]
-                 [org.clojure/java.jdbc "0.7.3"]
-                 [mysql/mysql-connector-java "5.1.18"]
-                 [com.mchange/c3p0 "0.9.5.2"] ;; connection pooling
+  :dependencies [[com.mchange/c3p0 "0.9.5.2"] ;; connection pooling
                  [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]
-                 [ring/ring-json "0.4.0"]
                  [instaparse "1.4.9"]
+                 [mysql/mysql-connector-java "5.1.18"]
+                 [org.clojure/clojure "1.8.0"]
                  [org.clojure/core.match "0.3.0-alpha5"]
-
-                 ]
+                 [org.clojure/java.jdbc "0.7.3"]
+                 [ring/ring-defaults "0.2.1"]
+                 [ring/ring-json "0.4.0"]]
   :plugins [[lein-ring "0.9.7"]
             ]
   :ring {:handler pine.handler/app}
   :profiles
   {
    :dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]
-                        ]}
+                        [ring/ring-mock "0.3.0"]]}
    :repl {
           :plugins [[cider/cider-nrepl "0.26.0"]
                     [refactor-nrepl "2.5.1"] ]
