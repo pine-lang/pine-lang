5d7cdd3e664d645f8826fd869a0e9a9109bc25f2
Ahmad Nazir Raja <ahmadnazir@gmail.com>
2024-06-19 23:24:27 +0200

    fix: fmt

--- src/pine/db/fixtures.clj
+++ src/pine/db/fixtures.clj
@@ -1,6 +1,5 @@
 (ns pine.db.fixtures)
 
-
 ;; Following tables exist:
 ;;
 ;;   `x`.`company`
--- test/pine/ast_test.clj
+++ test/pine/ast_test.clj
@@ -25,15 +25,11 @@
     (is (= 1
            (generate :limit "l: 1"))))
 
-
   (testing "Generate ast for `join` where there is no relation"
     (is (= {"a_0" {"b_1" nil}}
-           (generate :joins "a | b")))
-    )
+           (generate :joins "a | b"))))
 
   (testing "Generate ast for `join` where there is a relation"
     (is (= {"company_0" {"employee_1" ["employee_1" "company_id" := "company_0" "id"]}}
-           (generate :joins "company | employee")))
-    )
-  )
+           (generate :joins "company | employee")))))
 
