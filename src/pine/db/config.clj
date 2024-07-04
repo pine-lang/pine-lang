cb802ff18bd10a15b286aa397fbfb8736d13934b
Ahmad Nazir Raja <ahmadnazir@gmail.com>
2024-07-04 03:40:44 +0200

    fix: format

--- src/pine/db/config.clj
+++ src/pine/db/config.clj
@@ -3,17 +3,10 @@
 (defn get-env [var default]
   (or (System/getenv var) default))
 
-(def connections {
-                  :connection-id :default
-                  ;; :dev {:host "localhost" ;; use for docker: "host.docker.internal"
-                  ;;       :dbtype "postgres"
-                  ;;       :dbname "?"
-                  ;;       :user "?"
-                  ;;       :password "?"
-                  ;;       :schema nil}
+(def connections {:connection-id :default
                   :default {:host (get-env "DB_HOST" nil)
-                           :dbtype (get-env "DB_TYPE" "postgres")
-                           :dbname (get-env "DB_NAME" nil)
-                           :user (get-env "DB_USER" nil)
-                           :password (get-env "DB_PASSWORD" nil)
-                           :schema (get-env "DB_SCHEMA" nil)}})
+                            :dbtype (get-env "DB_TYPE" "postgres")
+                            :dbname (get-env "DB_NAME" nil)
+                            :user (get-env "DB_USER" nil)
+                            :password (get-env "DB_PASSWORD" nil)
+                            :schema (get-env "DB_SCHEMA" nil)}})
